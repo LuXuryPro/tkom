35a1360eba6a07fa62df6d398e4e258085a04c81
Radek <radek@rzaluska.com>
2016-05-06 20:56:58 +0200

    QueryLexer implementacja + testy

---
+++ src/query_lexer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lexer import Lexer
+
+
+class QueryLexerException(Exception):
+    pass
+
+
+class QueryLexer(Lexer):
+    def __init__(self, source) -> None:
+        super().__init__(source)
+
+    def _get_token(self) -> str:
+        """
+        private _get_token
+        """
+        c = self.source.next_char()
+        # go throught all spaces and return only one
+        if c == " ":
+            while c == " ":
+                c = self.source.next_char()
+
+            self.source.prev_char()
+            return " "
+        # one char tokens can be returned immediately
+        if c in [",", "(", ")", "\""]:
+            return c
+        if c == "~":
+            raise QueryLexerException("Unexpected ~ at {pos}".format(
+                pos=self.source.get_current_position()))
+        if c == "=":
+            n = self.source.next_char()
+            if n in ["=", "~"]:
+                return c + n
+            else:
+                # we have error \r is not beafore \n
+                raise QueryLexerException("Expected = or ~ at {pos} found '{c}'".format(
+                    pos=self.source.get_current_position(), c=n))
+
+        # digits
+        if c.isdigit():
+            return c
+
+        # collect all chars till whitespace or \n or digit
+        token_accumulator = []
+        token_accumulator.append(c)
+        while True:
+            c = self.source.next_char()
+            if c == "":
+                return c
+            if c in [",", "(", ")", "\"", " "]:
+                self.source.prev_char()
+                return "".join(token_accumulator)
+            token_accumulator.append(c)
+
---
+++ test/test_query_lexer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+
+from query_lexer import QueryLexer, QueryLexerException
+from source import Source
+
+
+def test_query_lexer_next_token():
+    source = Source('method if host == "google.pl"')
+    lexer = QueryLexer(source)
+    assert lexer.next_token() == "method"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "if"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "host"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "=="
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "\""
+    assert lexer.next_token() == "google.pl"
+    assert lexer.next_token() == "\""
+    assert lexer.next_token() == ""
+
+def test_query_lexer_prev_token():
+    source = Source('method if host == "google.pl"')
+    lexer = QueryLexer(source)
+    assert lexer.prev_token() == ""
+    assert lexer.next_token() == "method"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "if"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "host"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "=="
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "\""
+    assert lexer.next_token() == "google.pl"
+    assert lexer.next_token() == "\""
+    assert lexer.next_token() == ""
+    assert lexer.prev_token() == "\""
+    assert lexer.prev_token() == "google.pl"
+
+
+def test_query_lexer_exceptions():
+    source = Source('method if host = "google.pl"')
+    lexer = QueryLexer(source)
+    assert lexer.prev_token() == ""
+    assert lexer.next_token() == "method"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "if"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "host"
+    assert lexer.next_token() == " "
+    with pytest.raises(QueryLexerException) as e:
+        assert lexer.next_token() == "=="
+    assert str(e.value) == "Expected = or ~ at 16 found ' '"
