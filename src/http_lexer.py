466070c4e217b1779d90e8c376dac161568506b8
Radek <radek@rzaluska.com>
2016-05-06 10:30:12 +0200

    HTTPLexer - sklejanie ciągów spacji w jeden token - testy dla przypadków z dużą ilością spacji i dla pustego ciągu wejściowego - komentarze - usunięcie zbędnego zagnieżdżonego if dla HTTP - tesy dla nieprawidłowego tokent \r\n

--- src/http_lexer.py
+++ src/http_lexer.py
@@ -13,32 +13,28 @@ class HTTPLexer:
     def get_token(self):
         self.source.push_pos()
         c = self.source.next_char()
-        # go throught all spaces
-        while c == " ":
-            c = self.source.next_char()
-        # one char tokens
+        # go throught all spaces and return only one
+        if c == " ":
+            while c == " ":
+                c = self.source.next_char()
+            self.source.prev_char()
+            return " "
+        # one char tokens can be returned immediately
         if c in ["/", ".", ":"]:
             return c
+        # if next char is \n that means \r is missing beafore it and that is an error
         if c == "\n":
-            raise HTTPLexerException("Expected \\r at {pos} found \\n".format(
+            raise HTTPLexerException("Not expected \\n at {pos}".format(
                 pos=self.source.get_current_position()))
         if c == "\r":
-            # here w expect that next char is \n (LF)
+            # here we expect that next char is \n (LF)
             lf = self.source.next_char()
             if lf == "\n":
                 return "\r\n"
             else:
-                # we have error
+                # we have error \r is not beafore \n
                 raise HTTPLexerException("Expected \\n at {pos} found {c}".format(
                     pos=self.source.get_current_position(), c=lf))
-
-        self.source.push_pos()
-        if c == "H":
-            if self.source.next_char() == "T":
-                if self.source.next_char() == "T":
-                    if self.source.next_char() == "P":
-                        return "HTTP"
-        self.source.pop_pos()
 
         # digits
         if c.isdigit():
@@ -58,17 +54,21 @@ class HTTPLexer:
             token_accumulator.append(c)
 
     def next_token(self):
+        # save our positon for easy come back
         self.prev_tokens_start_pos.append(self.source.get_current_position())
         return self.get_token()
 
     def prev_token(self):
         if len(self.prev_tokens_start_pos) != 0:
+            # remove current token start pos from stack
             curr_pos = self.prev_tokens_start_pos.pop()
+            # get previous token start pos from stack
             prev_pos = self.prev_tokens_start_pos.pop()
             self.source.set_current_position(prev_pos)
             return self.get_token()
         else:
+            # we came back to begining
             self.source.set_current_position(-1)
             return ""
 
-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
--- test/test_http_lexer.py
+++ test/test_http_lexer.py
@@ -4,27 +4,57 @@ import pytest
 
 from http_lexer import HTTPLexer,HTTPLexerException
 from source import Source
+import pdb
 
 
 def test_lexer_get_next_token():
     source = Source("POST /plugins/phsys.php HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\n")
     lexer = HTTPLexer(source)
-    assert lexer.prev_token() == ""
     assert lexer.next_token() == "POST"
-    assert lexer.next_token() == "/"
-    assert lexer.next_token() == "plugins"
-    assert lexer.prev_token() == "/"
-
-def test_lexer_exceptions():
-    source = Source("POST /plugins/phsys.php HTTP/1.1\n")
-    lexer = HTTPLexer(source)
-    assert lexer.next_token() == "POST"
+    assert lexer.next_token() == " "
     assert lexer.next_token() == "/"
     assert lexer.next_token() == "plugins"
     assert lexer.next_token() == "/"
     assert lexer.next_token() == "phsys"
     assert lexer.next_token() == "."
     assert lexer.next_token() == "php"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "HTTP"
+    assert lexer.next_token() == "/"
+    assert lexer.next_token() == "1"
+    assert lexer.next_token() == "."
+    assert lexer.next_token() == "1"
+
+def test_lexer_get_next_token_big_spaces():
+    source = Source("POST                 /plugins/phsys.php                  HTTP/1.1                         \r\n")
+    lexer = HTTPLexer(source)
+    assert lexer.next_token() == "POST"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "/"
+    assert lexer.next_token() == "plugins"
+    assert lexer.next_token() == "/"
+    assert lexer.next_token() == "phsys"
+    assert lexer.next_token() == "."
+    assert lexer.next_token() == "php"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "HTTP"
+    assert lexer.next_token() == "/"
+    assert lexer.next_token() == "1"
+    assert lexer.next_token() == "."
+    assert lexer.next_token() == "1"
+
+def test_lexer_exceptions():
+    source = Source("POST /plugins/phsys.php HTTP/1.1\n\n")
+    lexer = HTTPLexer(source)
+    assert lexer.next_token() == "POST"
+    assert lexer.next_token() == " "
+    assert lexer.next_token() == "/"
+    assert lexer.next_token() == "plugins"
+    assert lexer.next_token() == "/"
+    assert lexer.next_token() == "phsys"
+    assert lexer.next_token() == "."
+    assert lexer.next_token() == "php"
+    assert lexer.next_token() == " "
     assert lexer.next_token() == "HTTP"
     assert lexer.next_token() == "/"
     assert lexer.next_token() == "1"
@@ -32,3 +62,22 @@ def test_lexer_exceptions():
     assert lexer.next_token() == "1"
     with pytest.raises(HTTPLexerException):
         assert lexer.next_token()
+
+def test_lexer_exceptions_second():
+    source = Source("n\n")
+    lexer = HTTPLexer(source)
+    assert lexer.next_token() == "n"
+    with pytest.raises(HTTPLexerException):
+        assert lexer.next_token()
+
+def test_lexer_exception_empty_next():
+    source = Source("")
+    lexer = HTTPLexer(source)
+    assert lexer.next_token() == ""
+    assert lexer.next_token() == ""
+
+def test_lexer_exception_empty_second():
+    source = Source("")
+    lexer = HTTPLexer(source)
+    assert lexer.prev_token() == ""
+    assert lexer.prev_token() == ""
